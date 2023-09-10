bc9249ab794ad43fde5ef1348a1d200851777070
OwinoMichael <owinomichael1400@gmail.com>
2023-09-10 10:05:55 +0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const firebaseConfig = {
+  apiKey: "AIzaSyAOO-Y9E0mfbMm2_F2WEGu5hXG9-CpjYUY",
+  authDomain: "url-shortener-67a0b.firebaseapp.com",
+  databaseURL: "https://url-shortener-67a0b-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "url-shortener-67a0b",
+  storageBucket: "url-shortener-67a0b.appspot.com",
+  messagingSenderId: "516320238802",
+  appId: "1:516320238802:web:a53906de30b998a2afe10b",
+  measurementId: "G-MKGRR5JLM0"
+};
+
+initializeApp(firebaseConfig);
+ReactDOM.render(
+  <BrowserRouter>
+      <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
